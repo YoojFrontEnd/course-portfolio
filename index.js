1cab03d798ea24376e18572463cb9d073867a3a8
Yevhen Galichenko <e.galichenko@gmail.com>
2022-05-08 19:39:43 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+let openModal = document.getElementById('userControlModalOpen');
+let closeModal = document.getElementById('userControlModalClose');
+let modalModal = document.getElementById('userControlModal');
 
+openModal.addEventListener('click', function (event) {
+  event.preventDefault();
+  console.log(openModal)
+  modalModal.style.display = 'block';
+  modalModal.style.transition = '0.3s all ease';
+
+});
+
+closeModal.addEventListener('click', function (event) {
+  event.preventDefault();
+  console.log(closeModal)
+  modalModal.style.display = 'none';
+});
---
+++ js/main.js
@@ -0,0 +1,14 @@
+let button = document.getElementById('open');
+let close = document.getElementById('close');
+let modal = document.getElementById('modal');
+
+button.addEventListener('click', function (event) {
+  event.preventDefault();
+  console.log(button)
+  modal.style.display = 'block';
+});
+
+close.addEventListener('click', function (event) {
+  event.preventDefault();
+  modal.style.display = 'none';
+});
